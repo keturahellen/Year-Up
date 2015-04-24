b9f8a4578e6574055609e7dc72c70cb86f7ea9e2
kjohnson116 <kjohnson@atl.yearup.org>
2015-04-24 16:03:12 -0400

    Project 1 for programming

---
+++ Project1/src/Intro.java
@@ -0,0 +1,15 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello! I'm Keturah Johnson. Welcome to the Java class.");
+	}
+	
+		
+
+}
+
+	/*Syntax error insert"}" to close body statement*/
+/*Syntax error, insert";" to complete BlockStatements*/
+//String literal is not properly closed by a double-quote//
