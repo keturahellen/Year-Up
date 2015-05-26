260536b9120bc57128fa8d4d9072cf905bbc322d
keturahellen <kjohnson@atl.yearup.org>
2015-05-25 22:58:43 -0400

    This program matches the chinese zodiac with birth year.

---
+++ project8/ChineseYear/src/ChineseYear.java
@@ -0,0 +1,104 @@
+
+public class ChineseYear {
+	static int s_knownYear=1900;
+
+	public static void main(String[] args){ 
+		// TODO Auto-generated method stub
+	}
+		
+	
+	void EvaluateChineseYear(int unknownYear)
+	
+	{
+		int yearDifference = unknownYear - s_knownYear;
+		int remainder = yearDifference % 12;
+		
+		if (unknownYear >=1900)
+		
+			switch(remainder)
+			{
+			case(0):
+			{	
+				System.out.println(unknownYear + " is the year of rat.");
+				break;
+			}
+			
+			case(1):
+			{
+				System.out.println(unknownYear + " is the year of ox.");
+				break;
+			}
+			
+			case(2):
+			{
+				System.out.println(unknownYear + " is the year of tiger.");
+				break;
+			}
+			
+			case(3):
+			{
+				System.out.println(unknownYear + " is the year of rabbit.");
+				break;
+			}
+			
+			case(4):
+			{
+				System.out.println(unknownYear + " is the year of dragon.");
+				break;
+
+			}
+			
+			case(5):
+			{
+				System.out.println(unknownYear + " is the year of snake.");
+				break;
+
+			}
+			
+			case(6):
+			{
+				System.out.println(unknownYear + " is the year of horse.");
+				break;
+
+			}
+			
+			case(7):
+			{
+				System.out.println(unknownYear + " is the year of sheep.");
+				break;
+
+			}
+			
+			case(8):
+			
+			{
+				System.out.println(unknownYear + " is the year of monkey.");
+				break;
+			}
+			
+			case(9):
+			{
+				System.out.println(unknownYear + " is the year of rooster.");
+				break;
+
+			}
+			
+			case(10):
+			{
+				System.out.println(unknownYear + " is the year of dog.");
+				break;
+
+			}
+		
+			case(11):
+			{
+				System.out.println(unknownYear + " is the year of pig.");
+				break;
+
+			}
+
+		
+		
+}
+}
+}
